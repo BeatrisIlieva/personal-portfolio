0340a967d07c83dc209b93acd516e0cfd46c0ff6
Beatris Ilieve <beatrisilieve@icloud.com>
2025-09-28 18:04:43 +0300

    added more ceritficates

--- src/components/main/ceritficates/Certificates.jsx
+++ src/components/main/ceritficates/Certificates.jsx
@@ -5,43 +5,82 @@ export const Certificates = () => {
         <section id='certificates' className={styles['certificates']}>
             <p>Programming Certificates</p>
             <div>
-                <a href='https://softuni.bg/certificates/details/248849/045919e0' target='blank'>
+                <a href='https://softuni.bg/certificates/details/250488/0b9adf9c' target='blank'>
+                    <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1759071463/ai-integrations_eweynq.png' />
+                </a>
+                <a
+                    href='https://softuni.bg/certificates/details/248849/045919e0'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1755098428/django-advanced_x9fig5.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/247278/39d45ee2' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/247278/39d45ee2'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1753787055/ai-tools-for-developers_ikpujs.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/246189/a46c73a2' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/246189/a46c73a2'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1751741336/django-basics_d6lpuh.png' />
                 </a>
-                {/* <a href='https://softuni.bg/certificates/details/247422/83fa7e0e'>
-                <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1758752590/247422_bdq0fz.png' />
-            </a> */}
-                <a href='https://softuni.bg/certificates/details/245061/89061a22' target='blank'>
+                <a target='blank' href='https://softuni.bg/certificates/details/247422/83fa7e0e'>
+                    <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1758752590/247422_bdq0fz.png' />
+                </a>
+                <a
+                    href='https://softuni.bg/certificates/details/245061/89061a22'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1750335746/typescript_j5qkfl.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/242263/8c6a2b9f' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/242263/8c6a2b9f'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1744638746/js-front-end_bytcnl.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/237923/b3ab2baa' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/237923/b3ab2baa'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/htm-and-css_vpqxxa.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/193787/03b13966' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/193787/03b13966'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/python-orm_yhdxlc.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/185983/50925b30' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/185983/50925b30'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/postgre-sql_rriyf4.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/180855/bb937ca4' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/180855/bb937ca4'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/python-oop_m4xe1d.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/173816/2932f905' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/173816/2932f905'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784910/python-advanced_nf2vyc.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/167232/525747d6' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/167232/525747d6'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/programming-fundamentals_iled2h.png' />
                 </a>
-                <a href='https://softuni.bg/certificates/details/143335/0a97348e' target='blank'>
+                <a
+                    href='https://softuni.bg/certificates/details/143335/0a97348e'
+                    target='blank'
+                >
                     <img src='https://res.cloudinary.com/dpgvbozrb/image/upload/v1743784909/programming-basics_wdkqsq.png' />
                 </a>
             </div>
